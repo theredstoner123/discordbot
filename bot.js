6730de9228497446b43eda74236bafcd1de7268c
theredstoner123 <32221378+theredstoner123@users.noreply.github.com>
2020-05-03 20:17:22 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,31 @@
-var Discord = require('discord.js');
-var bot = new Discord.Client({
-   token: process.env.BOT_TOKEN,
-   autorun: true
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+ 
+
+bot.on('ready', () => {
+
+    console.log('I am ready!');
+
 });
 
+ 
 
-bot.on('ready', function (evt) {
-    console.log('Connected' + '\n' + 'Logged in as: ' + '\n' + bot.username + ' - (' + bot.id + ')');
-});
+bot.on('message', (user, userID, channelID, message) => {
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
+    if (message.content === 'ping') {
+
+       bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
                 });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+
+       }
+
 });
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
