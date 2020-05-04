11091355638cab20ec22826e9f6dcf3555193f27
theredstoner123 <32221378+theredstoner123@users.noreply.github.com>
2020-05-03 20:27:26 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
  
 
-bot.on('ready', () => {
+client.on('ready', () => {
 
     console.log('Ready');
 
@@ -25,4 +25,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-bot.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
