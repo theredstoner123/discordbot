396e93207a272243d4c5c36a78a6fb092c1719af
theredstoner123 <32221378+theredstoner123@users.noreply.github.com>
2020-05-03 20:26:26 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,31 +5,20 @@ const bot = new Discord.Client();
 
 bot.on('ready', () => {
 
-    console.log('Connected, Logged in as: ' + bot.username + ' - (' + bot.id + ')');
+    console.log('Ready');
 
 });
 
  
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
 });
 
  
