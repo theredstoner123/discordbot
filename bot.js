30e77d7ff625cf43135fce655ea3eb02c002b8e3
theredstoner123 <32221378+theredstoner123@users.noreply.github.com>
2020-05-03 20:09:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
-var Discord = require('discord.io');
+var Discord = require('discord.js');
 var logger = require('winston');
-var auth = require('./auth.json');
+var bot = new Discord.Client({
+   process.env.BOT_TOKEN,
+   autorun: true
+});
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -8,15 +12,14 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 
-// Initialize Discord Bot
-var bot = new Discord.Client();
-bot.login(process.env.BOT_TOKEN);
+
 
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
