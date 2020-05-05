1c8985eef0e8b9f48acd602bf095c3bdc13c9383
theredstoner123 <32221378+theredstoner123@users.noreply.github.com>
2020-05-05 13:42:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client, MessageAttachment } = require("discord.js");
+const client = new Client();
 const cheerio = require("cheerio");
 const request = require("request");
 
