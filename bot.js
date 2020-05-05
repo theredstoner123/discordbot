09ad35e46d79b8cee3f6ff021495764fec8624cb
theredstoner123 <32221378+theredstoner123@users.noreply.github.com>
2020-05-05 13:31:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const GoogleImages = require("google-images");
-const googleImages = new GoogleImages("", "");
+const cheerio = require("cheerio");
+const request = require("request");
+
 
 var enabled = true;
 
@@ -74,7 +75,7 @@ client.on("message", message => {
 			}
 		}
 		message.channel.send(response);
-		googleImage(message)
+		image(message, response)
 	}
  }
  
@@ -100,19 +101,42 @@ function percentageSelect(percent, responses) {
 	return result;
 }
 
-async function googleImage(message) {
-  try {
-    const results = await googleImages.search(message.content);
-    const reply = !results.length ?
-      "No results" :
-      new Attachment(results[Math.floor(Math.random() * results.length)].url);
-    message.channel.send(reply);
-  }
-  catch (e) {
-    console.error(e);
-  }
+function image(message, search) {
+ 
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + search,
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            // handle error
+            return;
+        }
+ 
+        /* Extract image URLs from responseBody using cheerio */
+ 
+        $ = cheerio.load(responseBody); // load responseBody into cheerio (jQuery)
+ 
+        // In this search engine they use ".image a.link" as their css selector for image links
+        var links = $(".image a.link");
+ 
+        // We want to fetch the URLs not the DOM nodes, we do this with jQuery's .attr() function
+        // this line might be hard to understand but it goes thru all the links (DOM) and stores each url in an array called urls
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+        console.log(urls);
+        if (!urls.length) {
+            // Handle no results
+            return;
+        }
+ 
+        // Send result
+        message.channel.send(urls[0]);
+    });
 }
-
 
 // THIS  MUST  BE  THIS  WAY
 
