a7bbd9f50e81e0072a10a2affd91ffd6a308ccf1
theredstoner123 <32221378+theredstoner123@users.noreply.github.com>
2020-05-04 23:48:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,24 +13,38 @@ client.on('ready', () => {
 
 client.on('message', message => {
  
- if(message.channel.type === 'dm' && message.author.id === '338510413807353866' && message.content === 'off')
+ if(message.channel.type === 'dm' && message.author.id === '338510413807353866')
  {
-  enabled = false;
-  message.channel.send('off');
- }
- 
- if(message.channel.type === 'dm' && message.author.id === '338510413807353866' && message.content === 'on')
- {
-  enabled = true;
-  message.channel.send('on');
+     if(message.content === 'off')
+     {
+        enabled = false;
+        message.channel.send('off');
+     }
+     else if(message.content === 'on')
+     {
+        enabled = true;
+        message.channel.send('on');
+     }
+  
  }
 
  if(enabled === true){
-    if (message.author.id === '689560562513739957')
+    if (message.author.id === '689560562513739957')//issa
     {
-       message.channel.send('issa u cap');
+       if(new RegExp('d{1}[%](.*?)').match(message))
+       {
+            return 'bro ' + message.substring(0, 3) + '? that shit wack';
+       }
+       else if(new RegExp('d{2}[%](.*?)').match(message))
+       {
+            return 'bro ' + message.substring(0, 4) + '? that shit wack';
+       }
+       else if(new RegExp('d{3}[%](.*?)').match(message))
+       {
+            return 'bro ' + message.substring(0, 5) + '? that shit wack';
+       }
     }
-    else if (message.author.id === '707046570335535134')
+    else if (message.author.id === '707046570335535134')//assi
     {
        message.channel.send('assi u cap');
     }
@@ -38,7 +52,10 @@ client.on('message', message => {
  
 });
 
- 
+function arraySelect(array) {
+    return array[Math.round((Math.random()*(array.length)))];
+}
+
 
 // THIS  MUST  BE  THIS  WAY
 
