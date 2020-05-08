4e7175d9836b8078acab343b2db012079e122ea4
theredstoner123 <32221378+theredstoner123@users.noreply.github.com>
2020-05-08 00:37:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -199,7 +199,7 @@ function image(message, search) {
 			if(http.status === 200)
 			{
 				console.log(search + ":\t" + finalLink);
-				message.channel.send(search, {files: [{attachment: finalLink, name: search + finalLink.substring(finalLink.lastIndexOf("."), finalLink.length)}]}).catch(console.error).then((function (message) {message.react("❌")});
+				message.channel.send(search, {files: [{attachment: finalLink, name: search + finalLink.substring(finalLink.lastIndexOf("."), finalLink.length)}]}).catch(console.error).then(function (message) {message.react("❌")});
 				message.channel.stopTyping();
 				return;
 			}
